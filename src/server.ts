3f6e33c558e7df00e110db5ffa0994f06cf71fb1
Beatriz Felix <mbeatrizmfelix@gmail.com>
2021-03-21 21:56:40 -0300

    fix lint

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,16 @@
-import express from "express";
+import express from 'express'
 import cors from 'cors'
 
-const server = express();
-const port = 3000;
+const server = express()
+const port = 3000
 
-server.use(cors());
-server.use(express.json());
+server.use(cors())
+server.use(express.json())
 
-server.get("/", (_req, res) => {
-  res.json({ status: "server running" });
-});
+server.get('/', (_req, res) => {
+  res.json({ status: 'server running' })
+})
 
 server.listen(port, () => {
-  console.log(`listening on http://localhost:${port} 🚀`);
-});
-
-
-
+  console.log(`listening on http://localhost:${port} 🚀`)
+})
